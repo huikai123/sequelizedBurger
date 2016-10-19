0298aed0f718245fc1f62f052f301e6213bf9da2
Kai <huikaizhong@yahoo.com>
2016-10-19 17:24:11 -0400

    update

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,37 @@
+var burgers = require ('../models/')["burger"];
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+	res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+	burgers.findAll()
+	.then(function(burger_data){
+		console.log(burger_data);
+		return res.render('index', {burger_data});
+	})
+});
+
+router.post('/burgers/create', function (req, res) {
+	burgers.create({burger_name: req.boby.burger_name})
+	.then(function(newBurger){
+		console.log(newBurger);
+		res.redirect('/');
+	});
+});
+
+router.put('/burgers/update', function (req, res) {
+	burgers.findOne({where:{id:req.body.id}})
+	.then(function(thisBurger){
+		return thisBurger.updateAttributes({
+			devoured:true
+		}).then(function(){
+			res.redirect('/');
+		})
+	})
+});
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM burgers_db.burgers
+LIMIT 0, 5000
+
+-- Date: 2016-10-01 18:07
+*/
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1,'turkey', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2,'veggie', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3,'chips', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (4,'ham', false);
